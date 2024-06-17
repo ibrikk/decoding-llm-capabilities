85b5cdd8581093c975a1f7621fa8cc2abef42482
Ibrahim Khalilov <helmut.rozenweig@gmail.com>
2024-06-17 22:37:35 +0400

    add a virtual environment

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ python3 -m venv myenv
 # Activate virtual environment
 source myenv/bin/activate
 
-# Install dependencies
-pip install -r requirements.txt
+# # Install dependencies
+# pip install -r requirements.txt
 
 echo "Virtual environment setup complete. Use 'source myenv/bin/activate' to activate."
